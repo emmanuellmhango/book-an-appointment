61d5c187b704a061bfa2db1ad3243bc962b2fffe
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-04-12 19:44:54 -0600

    Start sign up in front end

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Body from './components/Body';
-import Footer from './components/Footer';
+// import Main from './components/Main';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+// import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Body />} />
+        {/* <Route path="/main" element={<Main />} /> */}
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/SignUp.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../styles/signup.css';
+import { Link } from 'react-router-dom';
+
+const SignUpComponent = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSignUp = async () => {
+    try {
+      const response = await axios.post('http://localhost:3000/users', {
+        user: {
+          name,
+          email,
+          password,
+          password_confirmation: passwordConfirmation,
+        },
+      });
+      console.log('Sign up successful:', response.data);
+      // Update message state with the success message from response
+      setMessage(response.data.http_status.message || '');
+    } catch (error) {
+      console.error('Sign up failed:', error);
+      // Update message state with the error message from response
+      setMessage(error.response.data.http_status.errors || 'Sign up failed');
+    }
+  };
+
+  return (
+    <div className="sign-up-form">
+      {/* Render message */}
+      <p>{message}</p>
+      <input className="name" type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+      <input className="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input className="password" type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <input className="password-confirmation" type="password" placeholder="Password Confirmation" value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)} />
+      <button className="button" type="button" onClick={handleSignUp}>Sign Up</button>
+      <Link to="/signin">Sign In</Link>
+    </div>
+  );
+};
+
+export default SignUpComponent;
