adaca6bbe309bc67790d5bbdf5b843d39939f930
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-04-18 16:44:03 -0600

    Start styling doctor details

--- src/pages/DoctorDetails.js
+++ src/pages/DoctorDetails.js
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchDoctors } from '../redux/doctors';
-// import '../styles/doctorDetails.css';
+import SideBar from '../components/SideBar';
+import '../styles/doctorDetails.css';
 
 const DoctorDetails = () => {
   const doctors = useSelector((state) => state.doctors);
@@ -15,49 +16,42 @@ const DoctorDetails = () => {
     }
   }, [dispatch, length]);
   const { doctorId } = useParams();
-  console.log(doctors);
-  // Find the doctor object with matching id
-  console.log(doctorId);
   const doctor = doctors.find((doctor) => doctor.id === parseInt(doctorId, 10));
   console.log(doctor);
   if (!doctor) {
-    // If doctor not found, return a not found message
     return <p className="doctor-details-not-found">Doctor not found</p>;
   }
 
   return (
-    <div className="doctor-details-container">
-      <img className="doctor-details-image" src={doctor.photo} alt="doctor" />
-      <h2 className="doctor-details-name">
-        Dr.
-        {doctor.name}
-      </h2>
-      <p className="doctor-details-specialization">
-        Specialization:
-        {doctor.specialization}
-      </p>
-      <p className="doctor-details-city">
-        Based in:
-        {doctor.city}
-      </p>
-      <p className="doctor-details-bio">
-        Bio:
-        {doctor.bio}
-      </p>
-      <p className="doctor-details-availability">
-        Availability:
-        {doctor.availability}
-      </p>
-      <p className="doctor-details-contact">
-        Contact:
-        {doctor.contact}
-      </p>
-      <p className="doctor-details-email">
-        Email:
-        {doctor.email}
-      </p>
-      {/* Render additional details as needed */}
-      {/* Add styling and other UI components as needed */}
+    <div className="doctor-details">
+      <SideBar />
+      <div className="doctor-details-container">
+        <img className="doctor-details-image" src={doctor.photo} alt="doctor" />
+        <div className="doctor-data">
+          <h2 className="doctor-details-name">
+            Dr.
+            {' '}
+            {doctor.name}
+          </h2>
+          <p className="doctor-details-specialization">
+            Specialization:
+            {doctor.specialization}
+          </p>
+          <p className="doctor-details-city">
+            Based in:
+            {doctor.city}
+          </p>
+          <p className="doctor-details-fee">
+            Charging fee: $
+            {doctor.fee}
+          </p>
+          <p className="doctor-details-experience">
+            Experience:
+            {doctor.experience}
+            years
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
