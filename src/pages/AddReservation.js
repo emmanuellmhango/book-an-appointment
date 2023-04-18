eda43f1b240bfbfd167a9618eef1fb0bd918c798
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-04-18 00:45:07 -0400

    Add create reservation service

--- src/pages/AddReservation.js
+++ src/pages/AddReservation.js
@@ -1,27 +1,7 @@
 import React from 'react';
 
 const AddReservation = () => (
-  <div>
-    <h2>Reserve</h2>
-    <form>
-      <label htmlFor="doctor">
-        Doctor:
-        <select name="doctor" id="doctor">
-          <option value="1">Dr. John Doe</option>
-          <option value="2">Dr. Jane Doe</option>
-        </select>
-      </label>
-      <label htmlFor="date">
-        Date:
-        <input type="date" name="date" id="date" />
-      </label>
-      <label htmlFor="city">
-        City:
-        <input type="text" name="city" id="city" />
-      </label>
-      <button type="submit">Reserve</button>
-    </form>
-  </div>
+  <h2>Reserve</h2>
 );
 
 export default AddReservation;
