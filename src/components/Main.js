e197fc0e075eed9b179fb77524ca8b6ed2efa7ff
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-04-17 23:16:26 -0600

    Fix linters

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
 import '../styles/main.css';
 import { fetchDoctors } from '../redux/doctors';
 import SideBar from './SideBar';
