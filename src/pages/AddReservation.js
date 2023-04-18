ad3036187188d46dc969436c2694683c8da358b8
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-04-18 00:26:59 -0400

    Create form to add a reservation

--- src/pages/AddReservation.js
+++ src/pages/AddReservation.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const AddReservation = () => (
   <div>
-    <h1>Add Reservation</h1>
+    <h2>Reserve</h2>
+    <form>
+      <label htmlFor="doctor">
+        Doctor:
+        <select name="doctor" id="doctor">
+          <option value="1">Dr. John Doe</option>
+          <option value="2">Dr. Jane Doe</option>
+        </select>
+      </label>
+      <label htmlFor="date">
+        Date:
+        <input type="date" name="date" id="date" />
+      </label>
+      <label htmlFor="city">
+        City:
+        <input type="text" name="city" id="city" />
+      </label>
+      <button type="submit">Reserve</button>
+    </form>
   </div>
 );
 
